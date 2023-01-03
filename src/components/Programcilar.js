6ce6b48fb35b7b709a15f2e180e25691f3663585
murat-dede <murat1_dede@hotmail.com>
2023-01-03 17:28:35 +0300

    s6g1

--- src/components/Programcilar.js
+++ src/components/Programcilar.js
@@ -13,6 +13,7 @@ Yorumları takip edin.
 
 import React from 'react';
 /* ADIM 0  */
+import { useState } from 'react';
 
 
 // Bu değişkeni YALNIZCA bir state dilimini başlatmak için kullanın!
@@ -30,7 +31,8 @@ export const enIyilerListesi = [
 export default function Programcilar() {
   // İki state dilimine ihtiyacımız olduğundan, state hooku iki kez kullanmamız gerekecek..
   // Bir yanda programcılar listesi, diğer yanda öne çıkan programcının idsi.
-
+const [programciListesi,setProgramciListesi]=useState(enIyilerListesi);
+const [programciId,setProgramciId]=useState(null)
 	
   const oneCikaninIsmi = () => {
     // Bunu sona bırakın!
@@ -38,12 +40,19 @@ export default function Programcilar() {
     // Öne çıkan geliştiricinin _isim_ adını döndürmek için her iki state dilimini kullanacak.
     // Closureların güzelliği, argümanlar yoluyla bilgi enjekte etmeye gerek kalmadan programın 
 	// bu bölgesinden her iki state dilimini de "görebilmemiz"dir.
+  let isim='';
+  programciListesi.forEach((item)=>{
+    if (item.id===programciId){
+      isim=item.isim;
+    }
+  });
+  return isim;
   };
 
   const stil = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: 'royalblue', // 🤔 kutlarken renk gold'a dönecek
+    color: programciId===null ? 'royalblue': 'gold' // 🤔 kutlarken renk gold'a dönecek
   };
 
   return (
@@ -55,9 +64,9 @@ export default function Programcilar() {
           // Şöyle diyebiliriz: "aa bu çalışıyor!" Ama programcilar bir state diliminden gelmiyorsa,
           // asla yeni programci ekleyemeyiz, programcilari düzenleyemeyiz ya da silemeyiz. Düzeltin!
           " */
-          enIyilerListesi.map(dev =>
+          programciListesi.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.isim} <button onClick={() => {/* burada dev.id 'yi öne çıkan id'ye atayın */ }}>Kutla</button>
+              {dev.isim} <button onClick={() => {setProgramciId(dev.id);}}>Kutla</button>
             </div>
           )
         }
@@ -67,7 +76,7 @@ export default function Programcilar() {
           // Üçlüler, bir şeyin "gerçekliğine" bağlı olarak "bir şeyi veya diğerini" ifade etmek için harikadır..
           // Sözde-kod: öne çıkan true ise metin 1'i oluşturun, aksi takdirde metin 2'yi oluşturun..
           // Sabit kodlanmış false'u doğru değişkenle değiştirin.
-          false
+          programciId!==null
             ? `🎉 Hadi ${oneCikaninIsmi()}'ı kutlayalım! 🥳`
             : 'Harika bir programcı seçin'
         }
